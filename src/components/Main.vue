29ed6c37c0ad04a28302173bece8a524fc309bd3
kasraouifiras <firaskasraoui29@gmail.com>
2020-12-17 13:43:05 +0100

    change buttons size

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -32,12 +32,12 @@
         :callback="(val, index) => answer(index, val)"
       ></question>
       <div
-        class="w-full ltr flex items-center flex-col xs:flex-row justify-between p-5"
+        class="w-full ltr flex items-center  justify-between p-5"
       >
         <button
           type="submit"
           @click="decrement"
-          class="text-xl m-2 order-2 xs:order-1 md:text-3xl rounded-full px-14 py-2 bg-gradient-to-r from-transparent to-transparent hover:from-primary-light hover:to-primary-dark hover:text-white border-primary border-2 hover:border-transparent text-primary transition ease-in-out duration-300"
+          class="text-sm  2xs:text-base xs:text-xl  md:text-3xl rounded-full px-5 2xs:px-10 xs:px-14 py-1 sm:py-2 bg-gradient-to-r from-transparent to-transparent hover:from-primary-light hover:to-primary-dark hover:text-white border-primary border-2 hover:border-transparent text-primary transition ease-in-out duration-300"
         >
           {{ $t("back") }}
         </button>
@@ -49,7 +49,7 @@
           @click="
             () => (index == testQuestions.length - step ? send() : increment())
           "
-          class="text-xl m-2 order-1 xs:order-2 md:text-3xl rounded-full px-14 py-2 bg-gradient-to-r from-primary-light to-primary-dark text-white hover:border-primary border-2 border-transparent hover:text-primary hover:from-transparent hover:to-transparent transition ease-in-out duration-300"
+          class="text-sm  2xs:text-base xs:text-xl  md:text-3xl rounded-full px-5 2xs:px-10 xs:px-14 py-1 sm:py-2 bg-gradient-to-r from-primary-light to-primary-dark text-white hover:border-primary border-2 border-transparent hover:text-primary hover:from-transparent hover:to-transparent transition ease-in-out duration-300"
         >
           {{ index == testQuestions.length - step ? $t("submit") : $t("next") }}
         </button>
